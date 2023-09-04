0f43f8f6f5157a2c41b0bd717df1b4235173dd09
JReed <jason.reed@fayette.kyschools.us>
2023-09-04 16:02:02 -0400

    Ep 7 Make a Route

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+  
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            words words more words words words words words words words words words words more words words words words words words words words words words more words words words words words words words words words words more words words words words words words words words words words more words words words words words words words words words words more words words words words words words words words
+        </p>
+
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
-Route::get('learn', function () {
-    return view('learn');
+
+Route::get('post', function () {
+    return view('post');
 });
+
