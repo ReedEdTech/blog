9586ff5483c6b10e27de4f033c5a680acd818a54
JReed <jason.reed@fayette.kyschools.us>
2023-09-07 16:57:23 -0400

    Ep 33 Blog Post Page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,15 +4,15 @@
 <!-- header -->
 @include('_posts-header');
 
-        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-            @if( $posts->count() )  <!-- Don't choke on my empty array -->                
-                <x-posts-grid :posts="$posts" />
-            @else
-                <p class="text-center">No posts yet</p>
-            @endif
+        @if( $posts->count() )  <!-- Don't choke on my empty array -->                
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center">No posts yet</p>
+        @endif
 
-        </main>
+    </main>
 
 
 
