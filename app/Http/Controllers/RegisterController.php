350cc4381ee2cc544d7c187e89fd2037eb34959b
JReed <jason.reed@fayette.kyschools.us>
2023-09-11 11:42:10 -0400

    Ep 47 Failed Validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -18,8 +18,8 @@ class RegisterController extends Controller
         //if validation fails, Laravel will automatically redirect you to the form again
         $attributes = request()->validate([
             'name' => 'required|max:255|min:3',
-            'username' => 'required|max:255',
-            'email' => 'required|email|max:255',
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|max:255|min:7'
         ]);
         //*NOTE:  In User class we overrode the mutator for password
