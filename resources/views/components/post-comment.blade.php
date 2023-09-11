d2eef5be82323a6b32a938ff43b4c4f8e4d777cd
JReed <jason.reed@fayette.kyschools.us>
2023-09-11 14:38:57 -0400

    Ep 52 Post Comment HTML

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,14 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div style="flex-shrink: 0;">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted <time>8 months ago</time>
+            </p>
+        </header>
+        <p>This is my comment. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu elementum ipsum, gravida rutrum lorem. Aliquam vitae feugiat dui. Mauris sit amet ante ultrices, accumsan orci porta, scelerisque ligula. Donec consequat, magna id placerat tempor, ligula ligula porttitor eros, non porttitor diam felis ac nibh. Suspendisse facilisis augue eros, eu faucibus purus luctus a. Pellentesque volutpat nisi nisi. Nunc nec nisl eget augue aliquet porta eget ut nulla.</p>
+    </div>
+</article>
