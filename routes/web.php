17211615f5f9d97e276ea47423f42fa50982e283
JReed <jason.reed@fayette.kyschools.us>
2023-09-12 10:49:20 -0400

    Ep 59 Newsletter Form

--- routes/web.php
+++ routes/web.php
@@ -43,10 +43,11 @@ Route::post( 'login', [SessionsController::class, 'store'] )->middleware('guest'
 //comments
 Route::post( 'posts/{post:slug}/comments', [PostCommentsController::class, 'store'] )->middleware('auth');
 
-//playing with mailchimp
-Route::get( 'ping' , function(){
-    //require_once('/path/to/MailchimpMarketing/vendor/autoload.php');
 
+//playing with mailchimp
+Route::post( 'newsletter' , function(){
+    //require_once('/path/to/MailchimpMarketing/vendor/autoload.php');
+    request() ->validate(['email' => 'required|email']);
     $mailchimp = new \MailchimpMarketing\ApiClient();
     
     $mailchimp->setConfig([
@@ -57,10 +58,17 @@ Route::get( 'ping' , function(){
     //$response = $mailchimp->ping->get();
     //$response = $mailchimp->lists->getAllLists();
     //$response = $mailchimp->lists->getList("cb7acb323a");
-    $response = $mailchimp->lists->addListMember("cb7acb323a", [
-        "email_address" => "Shemar56@yahoo.com",
-        "status" => "subscribed",
-    ]);
-
-    ddd($response);
+    try{
+        $response = $mailchimp->lists->addListMember("cb7acb323a", [
+            "email_address" => request('email'),
+            "status" => "subscribed",
+        ]);    
+    }
+    catch( \Exception $e){
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
+    //ddd($response);
+    return redirect('/')->with( 'success', 'You are now signed up for our newsletter!' );
 });
